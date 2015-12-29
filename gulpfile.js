c4e1ef19f85aaede81a62ce9647e3aa6fb9bc008
Matt <contact@mattmcfarland.com>
2015-12-29 01:48:35 -0500

    first release

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/* Module Dependencies */
+const gulp = require('gulp');
+
+/* Task Dependencies */
+const bundle = require('./tasks/bundle');
+const bundlemin = require('./tasks/bundlemin');
+const bundledeps = require('./tasks/bundledeps');
+const sass = require('./tasks/sass');
+
+/**
+ * Task Definitions
+ */
+
+
+gulp.task('bundle-vendor', (done) =>
+  bundledeps('vendor', 'lib/public/js/vendor', done)
+);
+gulp.task('bundle', () =>
+  bundlemin('src/main', 'main', 'lib/public/js')
+);
+gulp.task('bundle-dev', () =>
+  bundle('src/main', 'main', 'lib/public/js')
+);
+gulp.task('sass', () =>
+  sass('src/style/main.scss', 'main', 'lib/public/style')
+);
+
+gulp.task('build', ['bundle-vendor', 'bundle', 'sass']);
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CheckboxWithLabel from './components/CheckboxWithLabel';
+
+ReactDOM.render(
+  <div>
+    <CheckboxWithLabel/>
+  </div>
+  , document.getElementsByTagName('main'));
