782d77239fa8134582d4012028269179207b0d45
Matt <contact@mattmcfarland.com>
2016-01-01 20:37:47 -0500

    add source links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,8 +7,18 @@ export class Layout extends React.Component {
       <div>
         <h1>codepix.io</h1>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Home (Post)</Link></li>
           <li><Link to="/list">List</Link></li>
+          <li>Source
+          (<a href="https://github.com/MattMcFarland/codepix-client"
+               target="blank">
+              client
+            </a>) ..
+          (<a href="https://github.com/MattMcFarland/codepix-server"
+              target="blank">
+            server
+          </a>)
+          </li>
         </ul>
         {this.props.children}
       </div>
